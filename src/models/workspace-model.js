cbd9032f536910826e90e948f2e8abaaa93da6be
mdashfaquemarch <kashfaque557@gmail.com>
2025-06-09 11:56:04 +0530

    added basice schema

---
+++ src/models/workspace-model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const workspaceSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Workspace name is required"],
+        unique: [true, "Workspace name is unique"]
+    },
+    description: {
+        type: String
+    },
+    members: [
+        {
+            memberId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            },
+            role: {
+                type: String,
+                enum: ["admin", "member"],
+                default: "member"
+            }
+        }
+    ],
+    joinCode: {
+        type: String,
+        required: [true, "Join code is required"]
+    },
+    channels: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "channel"
+        }
+    ]
+}, { timestamps: true });
+
+
+const Workspace = mongoose.model("Workspace", workspaceSchema);
+
+export default Workspace;
