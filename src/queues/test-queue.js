84ef0259253b007b680ae54a9db432a506db5b6d
mdashfaquemarch <kashfaque557@gmail.com>
2025-06-18 18:25:46 +0530

    feat: remove test queue implementation from the project

--- src/queues/test-queue.js
+++
@@ -1,9 +0,0 @@
-import Queue from 'bull'
-import { Config } from '../config/serverConfig.js';
-
-export default new Queue('testQueue', {
-  redis: {
-    host: Config.REDIS_HOST,
-    port: Config.REDIS_PORT
-  }
-}, );
