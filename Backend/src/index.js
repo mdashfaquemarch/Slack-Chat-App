362dd065fae689c9188a1bba98f88780f4fcbb1b
mdashfaquemarch <kashfaque557@gmail.com>
2025-06-29 14:21:58 +0530

    feat: add CORS support and update environment sample for frontend URL

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,6 +4,7 @@ import { createBullBoard } from '@bull-board/api';
 import { BullAdapter } from '@bull-board/api/bullAdapter.js';
 import { ExpressAdapter } from '@bull-board/express';
 import express from 'express';
+import cors from 'cors'
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 
@@ -14,6 +15,16 @@ import mailQueue from './queues/mail-queue.js';
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL, // or ['https://site1.com', 'https://site2.com']
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+};
+
+
+// cors
+app.use(cors(corsOptions))
 
 // common middlewares
 
